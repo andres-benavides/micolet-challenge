250b11b117da0fc88c5a8f76e920f93df2ffc5d9
andres-benavides <andre0190@gmail.com>
2023-09-22 16:01:15 -0500

    Add unit test for model

--- spec/models/newsletter_spec.rb
+++ spec/models/newsletter_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Newsletter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:newsletter) { described_class.new(email: 'test@example.com') }
+
+  it 'is valid with valid attributes' do
+    expect(newsletter).to be_valid
+  end
+
+  it 'is not valid without an email' do
+    newsletter.email = nil
+    expect(newsletter).not_to be_valid
+  end
+
+  it 'is not valid with a duplicate email' do
+    newsletter.save
+    duplicate_newsletter = described_class.new(email: 'test@example.com')
+    expect(duplicate_newsletter).not_to be_valid
+  end
+
+  it 'is not valid with an invalid email format' do
+    invalid_emails = %w[invalid_email @example.com test.com]
+    invalid_emails.each do |email|
+      newsletter.email = email
+      expect(newsletter).not_to be_valid
+    end
+  end
 end
